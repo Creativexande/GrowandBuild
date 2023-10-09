073ddd028ab844e0511b03b028454a6355332b00
Creativexande <147163893+Creativexande@users.noreply.github.com>
2023-10-09 16:57:16 -0300

    toggle menu

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -1,15 +1,11 @@
-// Função para clonar os serviços e preencher o espaço vazio
-function cloneServices() {
-  const container = document.querySelector('.container');
-  const services = document.querySelectorAll('.service');
-  const serviceWidth = services[0].offsetWidth + 20; // Largura do serviço mais a margem direita
+const menuMobile = document.querySelector('.mobile-menu');
 
-  // Verifique se a largura atual do container é menor que a largura dos serviços
-  while (container.offsetWidth < serviceWidth * services.length) {
-      const clone = services[0].cloneNode(true);
-      container.appendChild(clone);
+const menuShow = () => {
+  menuMobile.classList.toggle("open");
+  
+  if (menuMobile.classList.contains('open')) {
+    document.querySelector('.icon').src = 'img/iconmenu.svg';
+  } else {
+    document.querySelector('.icon').src = 'img/x.svg';
   }
-}
-
-// Chame a função para clonar os serviços
-cloneServices();
+};
